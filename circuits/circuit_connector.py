5ac3f783505d8096d6711d4844fd0f7a65b0bd37
jwl-7 <lucidrainbows@gmail.com>
2025-07-18 23:08:44 -0400

    Update circuit connector to be stateless

--- circuits/circuit_connector.py
+++ circuits/circuit_connector.py
@@ -12,42 +12,41 @@ class CircuitConnector:
     """
 
     def __init__(self):
-        self.circuits = []
+        pass
 
-    def connect(self, circuit: Circuit) -> None:
+    def connect(self, circuits: list[Circuit], circuit: Circuit) -> None:
         """Recursively fills the input and output connection for a single circuit."""
         if circuit.input and circuit.output:
             return
 
         a = circuit
-        b = random.choice(self.circuits)
+        b = random.choice(circuits)
 
         if a is b:
-            self.connect(a)
+            self.connect(circuits, a)
             return
-        elif not a.input and not b.output and b.input is not a:
+        if not a.input and not b.output and b.input is not a:
             a.input = b
             print(f'{b.color} -> {a.color}')
         elif not a.output and not b.input and b.output is not a:
             a.output = b
             print(f'{a.color} -> {b.color}')
         elif not circuit.input or not circuit.output:
-            self.connect(circuit)
+            self.connect(circuits, a)
 
-    def connect_all(self, circuit: Circuit|None = None) -> None:
+    def connect_all(self, circuits: list[Circuit], circuit: Circuit|None = None) -> None:
         """Recursively connects each circuit recursively in a loop."""
         if circuit is None:
-            circuit = self.circuits[0]
-        if self.check_connections():
+            circuit = circuits[0]
+        if self.check_connections(circuits):
             print('[ALL CIRCUIT CONNECTIONS ESTABLISHED]')
             return
+        self.connect(circuits, circuit)
+        self.connect_all(circuits, circuit.output)
 
-        self.connect(circuit)
-        self.connect_all(circuit.output)
-
-    def check_connections(self) -> bool:
+    def check_connections(self, circuits: list[Circuit]) -> bool:
         """Sends a duck and a penguin to check circuit connections."""
-        duck = penguin = self.circuits[0]
+        duck = penguin = circuits[0]
         while duck.output and penguin.output and penguin.output.output:
             if duck is penguin:
                 return True
